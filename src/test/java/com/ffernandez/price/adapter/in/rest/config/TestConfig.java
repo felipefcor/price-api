136ee6dbba41c30397541eac22471d5ab4ab7451
f.fernandez <felipe.fernandez@kairos.com>
2023-05-04 19:46:05 +0200

    test: refactor prices controller test to slice and add bad request test

--- src/test/java/com/ffernandez/price/adapter/in/rest/config/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ffernandez.price.adapter.in.rest.config;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.ActiveProfiles;
-
-@ComponentScan("com.ffernandez.price.adapter.in.rest.mapper")
-@ActiveProfiles("test")
-public class TestConfig {}
--- src/test/java/com/ffernandez/price/adapter/in/rest/controller/PricesControllerTest.java
+++ src/test/java/com/ffernandez/price/adapter/in/rest/controller/PricesControllerTest.java
@@ -1,50 +1,37 @@
 package com.ffernandez.price.adapter.in.rest.controller;
 
-import com.ffernandez.price.adapter.in.rest.config.TestConfig;
-import com.ffernandez.price.domain.BrandId;
-import com.ffernandez.price.domain.ProductId;
 import com.ffernandez.price.adapter.in.rest.mapper.out.PriceResponseMapper;
 import com.ffernandez.price.application.port.in.PriceCommand;
 import com.ffernandez.price.application.port.in.PriceUseCase;
-import org.junit.jupiter.api.BeforeEach;
+import com.ffernandez.price.domain.BrandId;
+import com.ffernandez.price.domain.ProductId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.atMost;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = {PricesController.class, TestConfig.class})
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = PricesController.class)
 class PricesControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private PricesController pricesController;
 
-    @Autowired
+    @MockBean
     private PriceResponseMapper priceResponseMapper;
     @MockBean
     private PriceUseCase priceUseCase;
-
-    @BeforeEach
-    void init() {
-        mockMvc =
-                MockMvcBuilders.standaloneSetup(pricesController)
-                        .build();
-    }
 
     @ParameterizedTest
     @CsvSource({
@@ -68,7 +55,57 @@ class PricesControllerTest {
                         .brandId(new BrandId(brandId))
                         .date(date)
                         .build()));
+    }
 
+    @Test
+    void getPricesWithProductIdAndBrandAndBadDateThrowAnError() throws Exception {
+        mockMvc.perform(get("/prices")
+                        .param("productId", "35455")
+                        .param("brandId", "1")
+                        .param("date", LocalDate.now().toString())
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest());
+
+        then(priceUseCase).should(atMost(0))
+                .getPrice(eq(PriceCommand.builder()
+                        .productId(new ProductId(35455L))
+                        .brandId(new BrandId(1L))
+                        .date(LocalDateTime.now())
+                        .build()));
+    }
+
+    @Test
+    void getPricesWithBadProductIdAndBrandAndDateThrowAnError() throws Exception {
+        mockMvc.perform(get("/prices")
+                        .param("productId", "string")
+                        .param("brandId", "1")
+                        .param("date", "2020-06-14T10:00:00.00")
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest());
+
+        then(priceUseCase).should(atMost(0))
+                .getPrice(eq(PriceCommand.builder()
+                        .productId(new ProductId(35455L))
+                        .brandId(new BrandId(1L))
+                        .date(LocalDateTime.parse("2020-06-14T10:00:00.00"))
+                        .build()));
+    }
+
+    @Test
+    void getPricesWithProductIdAndBadBrandAndDateThrowAnError() throws Exception {
+        mockMvc.perform(get("/prices")
+                        .param("productId", "35455")
+                        .param("brandId", "string")
+                        .param("date", "2020-06-14T10:00:00.00")
+                        .header("Content-Type", "application/json"))
+                .andExpect(status().isBadRequest());
+
+        then(priceUseCase).should(atMost(0))
+                .getPrice(eq(PriceCommand.builder()
+                        .productId(new ProductId(35455L))
+                        .brandId(new BrandId(1L))
+                        .date(LocalDateTime.parse("2020-06-14T10:00:00.00"))
+                        .build()));
     }
 
 }
